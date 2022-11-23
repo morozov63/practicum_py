cd9fb2b5f655b5641bdd1ddf59eecb265f49440d
Turist <morozov130863@yandex.ru>
2022-11-23 22:19:09 +0300

    Solution

--- seminar.py
+++ seminar.py
@@ -1,4 +1,6 @@
 
+#Задача 1
+
 a = int(input('Введите число от 1 до 7: '))
 if a in range(1,6):
  print('Будний день')
@@ -7,7 +9,21 @@ elif a in range(6,8):
 else:
  print("Неверное число")
 
- a = int(input('Введите координату Х: '))
+ #Задача 2 Тут я так и не понял, что нужно сделать :-(, 
+ # может быть что то в таком духе. После 2го семинара 
+ # надеюсь все будет понятно 
+
+X = input('True или False: ')
+Y = input('True или False: ')
+Z = input('True или False: ')
+if not(X or Y or Z) == (not X and not Y and not Z):
+    print(1)
+else:
+    print(0)
+
+    #Задача 3
+
+a = int(input('Введите координату Х: '))
 b = int(input('Введите координату Y: '))
 if a > 0 and b > 0:
     print (1)
@@ -20,8 +36,9 @@ elif a > 0 and b < 0:
 else:
     print('X и Y не равны 0!') 
 
+    #Задача 4
 
-    a = int(input('Введите число от 1 до 4: '))
+a = int(input('Введите число от 1 до 4: '))
 if a == 1:
     print( 'X > 0 and Y > 0')
 elif a == 2:
@@ -32,6 +49,8 @@ elif a == 4:
     print( 'X > 0 and Y < 0') 
 else:
     print('Введите число от 1 до 4: ')
+
+#Задача 5
 
 a = int(input('Введите координату Х1: '))
 b = int(input('Введите координату Y1: '))
