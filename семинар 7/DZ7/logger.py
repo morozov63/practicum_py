2b422781e75293ab726f26bf5b4687643b74e4d9
morozov63 <113028247+morozov63@users.noreply.github.com>
2022-12-16 14:32:52 +0300

    Delete logger.py

--- семинар 7/DZ7/logger.py
+++
@@ -1,6 +0,0 @@
-def get_data():
-   with open('lection_1/seminar7/base.txt', 'r') as data: # здесь, если в одной папке, наверное можно просто base.txt
-      base = data.read()
-      base1 = base.split(',')
-      return base1
-      
